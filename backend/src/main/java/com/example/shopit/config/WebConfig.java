ac327a32c2bbb6e84ef482e56bde1ac0b88d0c99
Akash Saminathan <akashsaminathan24@gmail.com>
2025-10-22 15:35:08 +0530

    Update WebConfig.java

--- backend/src/main/java/com/example/shopit/config/WebConfig.java
+++ backend/src/main/java/com/example/shopit/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // all endpoints
-                        .allowedOrigins("https://shop-it-kappa.vercel.app/")
+                        .allowedOrigins("https://shop-it-kappa.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
