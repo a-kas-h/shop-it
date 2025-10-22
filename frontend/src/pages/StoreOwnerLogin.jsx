5d01aea12cdfccbdb72c2719089cfac22ebeca26
Akash Saminathan <akashsaminathan24@gmail.com>
2025-10-22 21:40:22 +0530

    Update StoreOwnerLogin.jsx

--- frontend/src/pages/StoreOwnerLogin.jsx
+++ frontend/src/pages/StoreOwnerLogin.jsx
@@ -27,7 +27,7 @@ function StoreOwnerLogin() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Store Owner Login
+            Login
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Sign in to your store owner account
