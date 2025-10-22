cd78087cedd5c65a0a52ee232b732e8a77785ff7
Akash Saminathan <akashsaminathan24@gmail.com>
2025-10-22 22:32:00 +0530

    ip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://13.235.244.62:8081/",
+        target: "http://15.207.107.210:8081/",
         changeOrigin: true,
         secure: false,
       },
